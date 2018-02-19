12e04b467e4077bd3f5f0c921a959b7e18ad382b
egoing <egoing@gmail.com>
2018-02-19 23:20:26 +0900

    mysqli를 이용해서 데이터베이스에 접속하는 방법 예제

---
+++ insert.php
@@ -0,0 +1,8 @@
+<?php
+// $mysqli = mysqli_connect("example.com", "user", "password", "database");
+// $res = mysqli_query($mysqli, "SELECT 'Please, do not use ' AS _msg FROM DUAL");
+// $row = mysqli_fetch_assoc($res);
+// echo $row['_msg'];
+
+mysqli_connect("localhost", "root", "111111", "opentutorials");
+?>
