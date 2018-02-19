57bd6dd84cccfc147931f782e68518cc451c863d
egoing <egoing@gmail.com>
2018-02-19 23:19:31 +0900

    웹쪽의 기본 예제 추가

---
+++ index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>WEB</title>
+  </head>
+  <body>
+    <h1>WEB</h1>
+    <ol>
+      <li>HTML</li>
+    </ol>
+    <h2>Welcome</h2>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit
+  </body>
+</html>
