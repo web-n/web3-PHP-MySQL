0d2e145b9d7e86fca3177bc9c1ed0591db105301
egoing <egoing@gmail.com>
2018-02-20 11:06:13 +0900

    글 생성 기능 구현

---
+++ create.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>WEB</title>
+  </head>
+  <body>
+    <h1>WEB</h1>
+    <ol>
+      <li>HTML</li>
+    </ol>
+    <form action="process_create.php" method="POST">
+      <p><input type="text" name="title" placeholder="title"></p>
+      <p><textarea name="description" placeholder="description"></textarea></p>
+      <p><input type="submit"></p>
+    </form>
+  </body>
+</html>
