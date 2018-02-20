234ae445f078693025828c4493b72b0dd6f6a4f5
egoing <egoing@gmail.com>
2018-02-20 09:08:09 +0900

    불필요한 주석 제거

--- insert.php
+++ insert.php
@@ -1,9 +1,4 @@
 <?php
-// $mysqli = mysqli_connect("example.com", "user", "password", "database");
-// $res = mysqli_query($mysqli, "SELECT 'Please, do not use ' AS _msg FROM DUAL");
-// $row = mysqli_fetch_assoc($res);
-// echo $row['_msg'];
-
 $conn = mysqli_connect("localhost", "root", "111111", "opentutorials");
 $sql  = "
 	INSER INTO topic (
