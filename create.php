1be648e51745cb646c4474f4b80676b740c719c9
egoing <egoing@gmail.com>
2018-02-20 17:11:39 +0900

    글읽기 구현 2 - 상세보기

--- create.php
+++ create.php
@@ -1,3 +1,30 @@
+<?php
+$conn = mysqli_connect(
+  'localhost',
+  'root',
+  '111111',
+  'opentutorials');
+
+$sql = "SELECT * FROM topic";
+$result = mysqli_query($conn, $sql);
+$list = '';
+while($row = mysqli_fetch_array($result)) {
+  $list = $list."<li><a href=\"index.php?id={$row['id']}\">{$row['title']}</a></li>";
+}
+
+$article = array(
+  'title'=>'Welcome',
+  'description'=>'Hello, web'
+);
+if(isset($_GET['id'])) {
+  $sql = "SELECT * FROM topic WHERE id={$_GET['id']}";
+  $result = mysqli_query($conn, $sql);
+  $row = mysqli_fetch_array($result);
+  $article['title'] = $row['title'];
+  $article['description'] = $row['description'];
+}
+
+?>
 <!doctype html>
 <html>
   <head>
@@ -5,9 +32,9 @@
     <title>WEB</title>
   </head>
   <body>
-    <h1>WEB</h1>
+    <h1><a href="index.php">WEB</a></h1>
     <ol>
-      <li>HTML</li>
+      <?=$list?>
     </ol>
     <form action="process_create.php" method="POST">
       <p><input type="text" name="title" placeholder="title"></p>
--- index.php
+++ index.php
@@ -12,6 +12,18 @@ while($row = mysqli_fetch_array($result)) {
   $list = $list."<li><a href=\"index.php?id={$row['id']}\">{$row['title']}</a></li>";
 }
 
+$article = array(
+  'title'=>'Welcome',
+  'description'=>'Hello, web'
+);
+if(isset($_GET['id'])) {
+  $sql = "SELECT * FROM topic WHERE id={$_GET['id']}";
+  $result = mysqli_query($conn, $sql);
+  $row = mysqli_fetch_array($result);
+  $article['title'] = $row['title'];
+  $article['description'] = $row['description'];
+}
+
 ?>
 <!doctype html>
 <html>
@@ -20,12 +32,12 @@ while($row = mysqli_fetch_array($result)) {
     <title>WEB</title>
   </head>
   <body>
-    <h1>WEB</h1>
+    <h1><a href="index.php">WEB</a></h1>
     <ol>
       <?=$list?>
     </ol>
     <a href="create.php">create</a>
-    <h2>Welcome</h2>
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit
+    <h2><?=$article['title']?></h2>
+    <?=$article['description']?>
   </body>
 </html>
