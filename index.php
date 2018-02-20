6518e8e6956f3645b07b5c011c6efbe5663b4875
egoing <egoing@gmail.com>
2018-02-20 16:52:21 +0900

    글읽기 구현 - 목록

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
+<?php
+$conn = mysqli_connect(
+  'localhost',
+  'root',
+  '111111',
+  'opentutorials');
+
+$sql = "SELECT * FROM topic";
+$result = mysqli_query($conn, $sql);
+$list = '';
+while($row = mysqli_fetch_array($result)) {
+  $list = $list."<li><a href=\"index.php?id={$row['id']}\">{$row['title']}</a></li>";
+}
+
+?>
 <!doctype html>
 <html>
   <head>
@@ -7,11 +22,7 @@
   <body>
     <h1>WEB</h1>
     <ol>
-      <li>HTML</li>
-      <li>CSS</li>
-      <li>JavaScript</li>
-      <li>PHP</li>
-      <li>MySQL</li>
+      <?=$list?>
     </ol>
     <a href="create.php">create</a>
     <h2>Welcome</h2>
