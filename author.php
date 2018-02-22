b889ccdad2996dc70580c771d55833c4bac9963e
egoing <egoing@gmail.com>
2018-02-22 17:56:22 +0900

    새로운 테이블 - 삭제

--- author.php
+++ author.php
@@ -32,6 +32,12 @@ $conn = mysqli_connect(
             <td><?=$filtered['name']?></td>
             <td><?=$filtered['profile']?></td>
             <td><a href="author.php?id=<?=$filtered['id']?>">update</a></td>
+            <td>
+              <form action="process_delete_author.php" method="post" onsubmit="if(!confirm('sure?')){return false;}">
+                <input type="hidden" name="id" value="<?=$filtered['id']?>">
+                <input type="submit" value="delete">
+              </form>
+            </td>
           </tr>
           <?php
         }
