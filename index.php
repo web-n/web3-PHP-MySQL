51c719a1247b699e5490cae5af6db59654ce2b7f
egoing <egoing@gmail.com>
2018-02-22 15:42:39 +0900

    관계형 데이터베이스 도입 - JOIN을 이용해서 글 읽기

--- index.php
+++ index.php
@@ -19,13 +19,16 @@ $article = array(
 );
 $update_link = '';
 $delete_link = '';
+$author = '';
 if(isset($_GET['id'])) {
   $filtered_id = mysqli_real_escape_string($conn, $_GET['id']);
-  $sql = "SELECT * FROM topic WHERE id={$filtered_id}";
+  $sql = "SELECT * FROM topic LEFT JOIN author ON topic.author_id = author.id WHERE topic.id={$filtered_id}";
+  
   $result = mysqli_query($conn, $sql);
   $row = mysqli_fetch_array($result);
   $article['title'] = htmlspecialchars($row['title']);
   $article['description'] = htmlspecialchars($row['description']);
+  $article['name'] = htmlspecialchars($row['name']);
 
   $update_link = '<a href="update.php?id='.$_GET['id'].'">update</a>';
   $delete_link = '
@@ -34,6 +37,7 @@ if(isset($_GET['id'])) {
       <input type="submit" value="delete">
     </form>
   ';
+  $author = "<p>by {$article['name']}</p>";
 }
 
 ?>
@@ -53,5 +57,6 @@ if(isset($_GET['id'])) {
     <?=$delete_link?>
     <h2><?=$article['title']?></h2>
     <?=$article['description']?>
+    <?=$author?>
   </body>
 </html>
